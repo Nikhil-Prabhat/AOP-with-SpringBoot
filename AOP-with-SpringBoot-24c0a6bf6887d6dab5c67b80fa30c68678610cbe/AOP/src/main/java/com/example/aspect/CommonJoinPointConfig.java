f5953539b7a2ec900c11d93d0694a5248370a433
Nikhil-Prabhat <62531542+Nikhil-Prabhat@users.noreply.github.com>
2024-04-23 12:44:58 +0530

    Update comments for pointcuts reusability

--- AOP-with-SpringBoot-24c0a6bf6887d6dab5c67b80fa30c68678610cbe/AOP/src/main/java/com/example/aspect/CommonJoinPointConfig.java
+++ AOP-with-SpringBoot-24c0a6bf6887d6dab5c67b80fa30c68678610cbe/AOP/src/main/java/com/example/aspect/CommonJoinPointConfig.java
@@ -6,6 +6,8 @@ public class CommonJoinPointConfig {
 
 	// To make the pointcuts reusable
 	// If we want to intercept across two layers , we can join the cuts with &&
+	// Supppose there is a package refactoring , in that case, we would need to change in multiple files, which is not expected.
+	// We can reuse pointcuts to minimise the changes
 
 	@Pointcut("execution(* com.example.business.*.*(..))")
 	public void businessLayerExecution() {
